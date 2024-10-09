f22353a6aa704839c9c0d1b796e048852268875f
Fermented-coder <kuivalappa@gmail.com>
2024-10-09 11:56:51 +0300

    nav bar bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,12 +10,16 @@ export const Navbar = () => {
           RE<span className="md:text-6xl text-5xl ">MA</span>FLIX
         </h1>
         <div className="text-white text-[19px] md:block hidden ">
-          <button className=" py-2 px-5 mx-5 border hover:opacity-65 duration-200  ">
-            <Link to="/login">Log In</Link>
-          </button>
-          <button className="py-2 px-5  mr-5 border-[#B8001F]  bg-[#B8001F] hover:opacity-65 duration-200">
-            <Link to="/signup">Sign Up</Link>
-          </button>
+          <Link to="/login">
+            <button className=" py-2 px-5 mx-5 border hover:opacity-65 duration-200  ">
+              Log In
+            </button>
+          </Link>
+          <Link to="/signup">
+            <button className="py-2 px-5  mr-5 border-[#B8001F]  bg-[#B8001F] hover:opacity-65 duration-200">
+              Sign Up
+            </button>
+          </Link>
         </div>
         <div className="mx-5 md:hidden block ">
           <Link to="/login" className="hover:text-gray-500">
